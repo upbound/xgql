7e479621c3b00a1779e50b8f737841cfbaede04b
Taylor Thornton <thornton.tn@gmail.com>
2022-12-17 20:16:54 -0800

    Extend error presentation to account for other error types - introduce new NOT_FOUND_ERROR  code - introduce new RETRYABLE_ERROR code - represent NoMatchKind and NoMatchResource differently so clients can distinguish between the two
    Signed-off-by: Taylor Thornton <thornton.tn@gmail.com>

--- internal/graph/present/present_test.go
+++ internal/graph/present/present_test.go
@@ -16,22 +16,29 @@ package present
 
 import (
 	"context"
+	"errors"
+	"syscall"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/pkg/errors"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	meta "k8s.io/apimachinery/pkg/api/meta"
 
 	"github.com/crossplane/crossplane-runtime/pkg/test"
 )
 
 func TestError(t *testing.T) {
 	errTimeout := kerrors.NewTimeoutError("too slow", 0)
+	errNetwork := syscall.ECONNREFUSED
+	errNoKindMatch := &meta.NoKindMatchError{}
 	errBoom := errors.New("boom")
 
 	gerrTimeout := gqlerror.WrapPath(nil, errTimeout)
+	gerrNetwork := gqlerror.WrapPath(nil, errNetwork)
+	gerrNoKindMatch := gqlerror.WrapPath(nil, errNoKindMatch)
 	gerrBoom := gqlerror.WrapPath(nil, errBoom)
+	gqlInput := "input: "
 
 	type args struct {
 		ctx context.Context
@@ -53,13 +60,35 @@ func TestError(t *testing.T) {
 				Message: errRBAC + ": " + gerrTimeout.Message,
 			},
 		},
+		"NetworkError": {
+			reason: "Network related errors should be 'upgraded' to a GQL error. ",
+			args: args{
+				ctx: context.Background(),
+				err: gerrNetwork,
+			},
+			want: &gqlerror.Error{
+				Message: gqlInput + gerrNetwork.Message,
+			},
+		},
+		"NotFoundError": {
+			reason: "Error types classified as 'Not Found' should be 'upgraded' to a GQL error. ",
+			args: args{
+				ctx: context.Background(),
+				err: gerrNoKindMatch,
+			},
+			want: &gqlerror.Error{
+				Message: gqlInput + gerrNoKindMatch.Message,
+			},
+		},
 		"OtherGQLError": {
 			reason: "Regular GQL errors should be returned unchanged.",
 			args: args{
 				ctx: context.Background(),
 				err: gerrBoom,
 			},
-			want: gerrBoom,
+			want: &gqlerror.Error{
+				Message: gqlInput + gerrBoom.Message,
+			},
 		},
 		"OtherTimeoutError": {
 			reason: "Non-GQL timeout errors should be both decorated and 'upgraded' to a GQL error.",
